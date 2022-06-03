f81c4ffc946036d3f2719aa886549a6bb2d9d717
Cleyton Costa <cleytonotyelc1995@gmail.com>
2022-06-03 16:16:01 -0300

    Add files via upload

---
+++ web/src/components/Widget.tsx
@@ -0,0 +1,44 @@
+import { Popover } from '@headlessui/react';
+import { ChatTeardropDots } from 'phosphor-react';
+import { WidgetForm } from './WidgetForm';
+
+function Widget() {
+  return (
+    <Popover
+      className="
+        grid justify-items-end
+        fixed bottom-4 right-4 sm:bottom-8 sm:right-8
+      "
+    >
+      <Popover.Panel>
+        <WidgetForm />
+      </Popover.Panel>
+
+      <Popover.Button
+        className="
+          flex group
+          p-3
+          rounded-full
+          bg-brand-500 hover:bg-brand-400
+          text-white
+          transition-colors
+        "
+      >
+        <ChatTeardropDots className="w-6 h-6" />
+
+        <span
+          className="
+            max-w-0 group-hover:max-w-xs
+            overflow-hidden
+            transition-all duration-500 ease-linear
+          "
+        >
+          <span className="ml-2"></span>
+          Feedback
+        </span>
+      </Popover.Button>
+    </Popover>
+  );
+}
+
+export { Widget };
