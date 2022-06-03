cec39a27ee069a996a1864e854b7e080d3937971
Cleyton Costa <cleytonotyelc1995@gmail.com>
2022-06-03 16:26:03 -0300

    Add files via upload

---
+++ mobile/src/components/Success/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+
+import SuccessImg from '../../assets/success.png';
+import { Copyright } from '../Copyright';
+
+import { styles } from './styles';
+
+interface Props {
+  onSendAnotherFeedback: () => void;
+}
+
+export function Success({ onSendAnotherFeedback }: Props) {
+  return (
+    <View style={styles.container}>
+        <Image 
+        source={SuccessImg}
+        style={styles.image}/>
+
+        <Text style={styles.title}>
+            Agradecemos seu feedback
+        </Text>
+
+        <TouchableOpacity 
+        style={styles.button}
+        onPress={onSendAnotherFeedback}>
+            <Text style={styles.buttonTitle}>
+                Quero enviar outro feedback
+            </Text>
+        </TouchableOpacity>
+
+        <Copyright/>
+    </View>
+  );
+}
